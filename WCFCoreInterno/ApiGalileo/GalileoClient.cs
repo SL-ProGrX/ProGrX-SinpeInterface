7f92a8d1f6cdac23e338580bdae0ea9ae1fd8e07
David Gutierrez M <164664936+dgutierrezm90@users.noreply.github.com>
2025-11-21 07:19:39 -0600

    Primera Carga
    Interface Core SINPE con Prosoft

---
+++ WCFCoreInterno/ApiGalileo/GalileoClient.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFCoreInterno.ApiGalileo
+{
+    public class GalileoClient
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+        private readonly string galileoUri;
+        public readonly string _token;
+
+        public GalileoClient()
+        {
+            galileoUri = System.Configuration.ConfigurationManager.AppSettings["GalileoUri"];
+            _token = System.Configuration.ConfigurationManager.AppSettings["GalileoToken"];
+        }
+
+        public async Task<bool> ServicioDisponible(int CodEmpresa)
+        {
+            var rersponse = false;
+            try
+            {
+                _httpClient.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", _token);
+
+                var response = await _httpClient.GetAsync($"{galileoUri}/api/mKindoService/ServicioDisponible/{CodEmpresa}");
+
+                response.EnsureSuccessStatusCode();
+
+                var json = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<bool>(json);
+            }
+            catch (System.Exception)
+            {
+                rersponse = false;
+            }
+
+            return rersponse;
+        }
+
+        public async Task<CuentaIBAN_Response> ObtenerCuentaIBAN(int CodEmpresa, CuentaIBAN_Request DatosCuenta)
+        {
+            var rersponse = new CuentaIBAN_Response();
+            try
+            {
+                _httpClient.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", _token);
+
+                var jsonContent = new StringContent(
+                    JsonConvert.SerializeObject(DatosCuenta),
+                    Encoding.UTF8,
+                    "application/json");
+
+                var response = await _httpClient.PostAsync(
+                         $"{galileoUri}/api/mKindoService/ObtenerCuentaIBAN/{CodEmpresa}",
+                         jsonContent
+                     );
+
+                response.EnsureSuccessStatusCode();
+
+                var json = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<CuentaIBAN_Response>(json);
+            }
+            catch (System.Exception)
+            {
+                rersponse = new CuentaIBAN_Response()
+                { 
+                    CuentaIBAN = null,
+                    Errores = new List<CL_Error> { 
+                        new CL_Error() 
+                        { 
+                            NumError = -1, 
+                            Descripcion = "Error de comunicación con el servicio Galileo" 
+                        } 
+                    },
+                    Resultado = false
+                };
+            }
+
+            return rersponse;
+        }
+
+    }
+}
---
+++ WCFCoreInterno/CoreInternoServicio.svc.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using WCFCoreInterno.ApiGalileo;
+
+namespace WCFCoreInterno
+{
+    public class CoreInterno : ICoreInterno
+    {
+
+        GalileoClient galileo = new GalileoClient();
+
+        public bool ServicioDisponible(int CodEmpresa)
+        {
+            return galileo.ServicioDisponible(CodEmpresa).Result;
+        }
+
+        public CL_ResultadoValidacion[] ValidaDebitos(SI_Rastro rastro, CL_DatosTransaccion[] transacciones)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public CL_ResultadoValidacion[] ValidaCreditos(SI_Rastro rastro, CL_DatosTransaccion[] transacciones)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public CL_ValidaCuenta ValidaCuenta(string Identificacion, string CuentaIBAN, int CodigoMoneda)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValidacionPerfilTrx_Response ValidarPerfilTransaccional(ValidacionPerfilTrx_Request transaccion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CuentaIBAN_Response ObtenerCuentaIBAN(int CodEmpresa, CuentaIBAN_Request DatosCuenta)
+        {
+            return galileo.ObtenerCuentaIBAN(CodEmpresa, DatosCuenta).Result;
+        }
+
+        public CL_ObtieneInfoCuenta ObtieneInfoCuenta(string Identificacion, string CuentaIBAN)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
+        ObtieneEstadoTransaccionResponse ICoreInterno.ObtieneEstadoTransaccion(ObtieneEstadoTransaccionRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public CL_RespuestaTransaccion[] AplicaDebitosCongelados(SI_Rastro rastro, CL_Transaccion[] transacciones)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public CL_RespuestaTransaccion[] AplicaCreditosCongelados(SI_Rastro rastro, CL_Transaccion[] transacciones)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public CL_ResultadoActualizacion[] ConfirmaCreditosCongelados(SI_Rastro rastro, CL_ActualizaTransaccion[] transacciones)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public CL_ResultadoActualizacion[] ConfirmaDebitosCongelados(SI_Rastro rastro, CL_ActualizaTransaccion[] transacciones)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public CL_RespuestaTransaccion[] AplicaTransferenciasFirmes(SI_Rastro rastro, CL_Transaccion[] transacciones)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
+        SaldoDisponibleResponse ICoreInterno.SaldoDisponible(SaldoDisponibleRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
+        ComisionRespectivaResponse ICoreInterno.ComisionRespectiva(ComisionRespectivaRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public CL_ResultadoActualizacion[] ReversaCreditos(SI_Rastro rastro, TransaccionRechazada[] transacciones)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public CL_ResultadoActualizacion[] ReversaDebitos(SI_Rastro rastro, TransaccionRechazada[] transacciones)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
+        ObtenerInformacionClienteResponse ICoreInterno.ObtenerInformacionCliente(ObtenerInformacionClienteRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
+        ObtenerProductosPorClienteResponse ICoreInterno.ObtenerProductosPorCliente(ObtenerProductosPorClienteRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public CL_ResultadoTipoCambio ObtenerTipoCambio(SI_Rastro Rastro, int CodigoServicio, string CuentaOrigen, string CuentaDestino, decimal Monto, int Moneda)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ActualizarFechaCiclo(int ComprobanteCGP, string DocumentoSistemaInterno, int ServicioSINPE, System.DateTime FechaCiclo, string CodigoReferenciaAnterior, string CodigoReferenciaNuevo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool LiquidarCiclo(int[] EntidadesAplazadas, int ServicioSINPE, string Modalidad, System.DateTime FechaCiclo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CL_RespuestaNotificacion[] PreferenciasNotificacion(SI_Rastro rastro, CL_Notificacion[] notificacion)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
